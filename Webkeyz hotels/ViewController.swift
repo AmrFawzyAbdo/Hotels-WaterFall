71d77d09cf9198c31265f0bc1a6df837829b2795
Amr <amrfawz7.0794@gmail.com>
2021-01-27 05:53:55 +0200

    Initial Commit

---
+++ Webkeyz hotels/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Webkeyz hotels
+//
+//  Created by Amr Fawzy on 1/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
